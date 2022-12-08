db22f13d6f4085f9ee50fc13da81fad97f4628f5
pramith <malagodap2@gmail.com>
2022-12-08 22:54:03 +0530

    Js color change added..

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const color = document.querySelector('.color');
 btn.addEventListener('click', function() {
     //console.log(document.body);
 
-    //Get random number between 0-3
-    const randomNumber = 2;
+    //Get random number between 0-3 colors
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 })
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
